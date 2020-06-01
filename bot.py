91dde36aba8366e9638880bb61d6aaa379c381b1
Dilyara.Islamova <d.lyara@bk.ru>
2020-06-01 21:57:37 +0300

    mybot

--- bot.py
+++ bot.py
@@ -13,19 +13,32 @@ def greet_user(update, context):
     update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
 
 def talk_to_me(update, context):
-    user_text = update.message.text 
-    print(user_text)
-    update.message.reply_text(user_text)    
+    text = update.message.text 
+    print(text)
+    update.message.reply_text(text)  
+
+def guess_number(update, context): 
+    print(context.args)
+    if context.args:
+       try: 
+           user_number = int(context.args[0])
+           message = f"Вы ввели число {user_number}"
+       except (TypeError, ValueError):
+           message = "Введите целое число" 
+    else:
+        message = "Введите число"
+    update.message.reply.text(message)       
 
 def main():
     mybot = Updater(settings.API_KEY , use_context=True, request_kwargs=PROXY)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("guess", guess_number))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
  
     logging.info("Бот стартовал")
     mybot.start_polling()
     mybot.idle()
 if __name__ == "__main__":
-main()
+    main()
