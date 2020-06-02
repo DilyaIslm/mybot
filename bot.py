b7f6cf64ba83017f2c28ae50a02325c8eecbc9ea
Dilyara.Islamova <d.lyara@bk.ru>
2020-06-02 21:57:18 +0300

    mybot

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
+from glob import glob
 import logging
+
+from random import randint, choice
+
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import settings
@@ -15,19 +19,36 @@ def greet_user(update, context):
 def talk_to_me(update, context):
     text = update.message.text 
     print(text)
-    update.message.reply_text(text)  
+    update.message.reply_text(text) 
+
+def play_random_number(user_number):
+    bot_number = randint(user_number - 10, user_number + 10)
+    if user_number > bot_number:
+        message = f"Ваше число {user_number}, а мое число {bot_number}, Вы выиграли"
+    elif user_number == bot_number:
+        message = f"Ваше число {user_number}, а мое число {bot_number}, ничья"
+    else:
+        message = f"Ваше число {user_number}, а мое число {bot_number}, Вы проиграли"
+    return message        
 
 def guess_number(update, context): 
     print(context.args)
     if context.args:
        try: 
            user_number = int(context.args[0])
-           message = f"Вы ввели число {user_number}"
+           message = play_random_number(user_number)
        except (TypeError, ValueError):
            message = "Введите целое число" 
     else:
         message = "Введите число"
-    update.message.reply.text(message)       
+    update.message.reply_text(message)
+
+def send_cat_picture(update, context):
+    cat_photo_list = glob("images/cat*.jp*g")
+    cat_photo_filename = choice(cat_photos_list)
+    chat_id = update.effective_chat.id
+    context.bot.send_photo(chat_id=chat_id, photo=open(cat_photo_filename, "rb"))
+
 
 def main():
     mybot = Updater(settings.API_KEY , use_context=True, request_kwargs=PROXY)
@@ -35,6 +56,7 @@ def main():
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("guess", guess_number))
+    dp.add_handler(CommandHandler("cat", send_cat_picture))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
  
     logging.info("Бот стартовал")
